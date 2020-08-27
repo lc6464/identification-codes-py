f6357d0717bc22a1ebfcb6434ecf30bf27f170fd
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-08-27 21:48:26 +0800

    优化注释和 README

--- program.py
+++ program.py
@@ -10,50 +10,50 @@ headers = {
 
 class GetCode:
 	def __init__(self, path, url=None, headers=headers, origin='chaxun.heyuanedu.cn:88', threshold=185): # 初始化对象实例
-		self.path = path
+		self.path = path # 赋值变量
 		self.url = url
 		self.headers = headers
 		self.origin = origin
 		self.threshold = threshold
 
 	def getReq(self, url=None, headers=None, origin='chaxun.heyuanedu.cn:88'): # 获取 Request 对象
-		self.url = url if url != None else self.url
+		self.url = url if url != None else self.url # 赋值变量
 		self.headers = headers if headers != None else self.headers
 		self.origin = origin if origin != None else self.origin
-		return Request(self.url, None, self.headers, self.origin)
+		return Request(self.url, None, self.headers, self.origin) # 返回 Request 对象
 
 	def setting(self, path=None, url=None, headers=None, origin=None, threshold=None): # 设置实例的各类变量
-		self.path = path if path != None else self.path
+		self.path = path if path != None else self.path # 赋值变量
 		self.url = url if url != None else self.url
 		self.headers = headers if headers != None else self.headers
 		self.origin = origin if origin != None else self.origin
 		self.threshold = threshold if threshold != None else self.threshold
 
 	def getSession(self, url=None, headers=None, origin=None): # 获取对应系统的 Session ，注意：请勿重复调用！否则将可能导致不可预知的问题！
-		self.url = url if url != None else self.url
+		self.url = url if url != None else self.url # 赋值变量
 		self.headers = headers if headers != None else self.headers
 		self.origin = origin if origin != None else self.origin
-		self.headers['Cookie'] = urlopen(self.getReq()).info()['Set-Cookie'].split(';')[0]
-		return self.headers
+		self.headers['Cookie'] = urlopen(self.getReq()).info()['Set-Cookie'].split(';')[0] # 写入 Cookie（无法处理多个 Cookies 的情况！如果重复调用也可能导致出现问题）（其实就是因为我懒）
+		return self.headers # 返回 headers
 
 	def download(self, url=None, path=None, headers=None, origin=None): # 下载验证码图片
-		self.url = url if url != None else self.url
+		self.url = url if url != None else self.url # 赋值变量
 		self.path = path if path != None else self.path
 		self.headers = headers if headers != None else self.headers
 		self.origin = origin if origin != None else self.origin
 		if not os.path.exists(self.path[:self.path.rfind(os.sep)]): # 防止目录不存在出错
-			os.mkdir(self.path[:self.path.rfind(os.sep)])
-		with open(self.path, 'wb') as f:
-			size = f.write(urlopen(self.getReq()).read())
-		return size
+			os.mkdir(self.path[:self.path.rfind(os.sep)]) # 创建目录
+		with open(self.path, 'wb') as f: # 开文件
+			size = f.write(urlopen(self.getReq()).read()) # 写文件（图片）
+		return size # 返回文件大小
 
 	def identify(self, path=None, threshold=None): # 识别验证码
-		self.path = path if path != None else self.path
+		self.path = path if path != None else self.path # 赋值变量
 		self.threshold = threshold if threshold != None else self.threshold
-		image = Image.open(self.path)
-		pixdata = image.load()
-		w, h = image.size
-		for y in range(h): # 处理多余色彩
+		image = Image.open(self.path) # PIL 打开图片
+		pixdata = image.load() # 载入图片到变量
+		w, h = image.size # 获取宽高
+		for y in range(h): # 处理色彩
 			for x in range(w):
 				if pixdata[x, y][0] < self.threshold and pixdata[x, y][1] < self.threshold and pixdata[x, y][2] < self.threshold:
 					pixdata[x, y] = (0, 0, 0)
