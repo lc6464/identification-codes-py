de7767ffd35daefc5bcac88f2ca8ac6517151e6e
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-08-27 21:48:26 +0800

    优化注释和 README

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# Python 验证码识别
+# 图形验证码识别系统
+- 由 Python 编写
+- 超级简单
+- 不怎么靠谱
+- 不建议用于生产环境
 
-> 由 Python 编写的超级简单的不怎么靠谱的验证码识别系统
-
-- 使用 PIL 处理验证码图片
+## 原理
+- 使用 `PIL` 处理验证码图片
 - 使用 ORC 方式识别验证码
+  - `pytesseract`
+
+
+## 文件
+- `program.py`：程序文件
+  - `class GetCode`
+  - `dict headers`
+  - 75行在生产环境下请删除或注释掉
+- `test.py`：测试文件
+  - 针对 [http://chaxun.heyuanedu.cn:88/](http://chaxun.heyuanedu.cn:88/ "http://chaxun.heyuanedu.cn:88/") 系统的测试
+  - 靠运气和大量尝试来尝试验证码
+  - 图片储存至 image/
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from urllib.parse import urlencode
 import re, os
 
 ele0 = ''
-p = re.compile(r'(\d)[^0-9]*(\d)[^0-9]*(\d)[^0-9]*(\d)') # 此系统验证码正则
+p = re.compile(r'(\d)[^0-9]*(\d)[^0-9]*(\d)[^0-9]*(\d)') # 验证码正则
 
 getCode = GetCode('image%scode.png'%os.sep, 'http://chaxun.heyuanedu.cn:88/validatecode.php?act=getimg') # 实例化对象
 getCode.getSession() # 获取 Session
@@ -15,24 +15,21 @@ getCode.getSession() # 获取 Session
 userName = input('姓名：')
 identity = input('准考证号：')
 
-while ele0 == '': # 识别通过正则但错误循环
+while ele0 == '': # 通过正则但错误
 	result2 = None
-	while result2 == None: # 识别无法通过正则循环
-		#time.sleep(0.2)
+	while result2 == None: # 无法通过正则  #time.sleep(0.2)
 		print('Downloading...')
-		getCode.download()
+		getCode.download() # 下载验证码
 		print('Identifying...')
-		result = getCode.identify()
-		result2 = ('%s%s%s%s'%p.search(result).groups() if p.search(result) != None else None)
-		result0 = (result2 if result2 != None else result) if result != '' else 'failure!'
+		result = getCode.identify() # 识别
+		result2 = ('%s%s%s%s'%p.search(result).groups() if p.search(result) != None else None) # 正则判断
+		result0 = (result2 if result2 != None else result) if result != '' else 'failure!' # 识别结果
 		print('Result: ' + result0)
 
 	req = Request('http://chaxun.heyuanedu.cn:88/search.php', urlencode({ 'userName': userName, 'identity': identity, 'code': result0 }).encode(), headers, 'chaxun.heyuanedu.cn:88')
 	res = urlopen(req) # 获取结果
-	html = res.read().decode()
-	#print(res.headers)
-	#print(html)
+	html = res.read().decode() # 获取结果  #print(res.headers) #print(html)
 	soup = BeautifulSoup(html,'lxml') # 解析 html
-	ele = soup.select('#printGrade')
-	ele0 = str(ele)[1:-1]
+	ele = soup.select('#printGrade') # 获取相应元素
+	ele0 = str(ele)[1:-1] # 获取结果(str)
 	print(ele0 if ele0 != '' else 'Error!')
